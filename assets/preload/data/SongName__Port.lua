0d87d27091bc7bd9df13cb0f95171d3c2f19d724
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-08-21 18:31:44 -0300

    Delete SongName__Port.lua

--- assets/preload/data/SongName__Port.lua
+++
@@ -1,5 +0,0 @@
-function onCreate()
-makeLuaText('watermark', songName .. " | Port by DANIZIN", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
-    setTextSize('watermark', 20);
-    addLuaText('watermark');
-end
