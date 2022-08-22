d2487731119d2ebd7b2c3e7cabba46408fbc48a2
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-08-22 06:43:46 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -14,9 +14,7 @@ import flixel.FlxBasic;
 import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.FlxGame;
-AddAndroidControls
 import flixel.FlxObject;
-AndroidControls
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.FlxSubState;
